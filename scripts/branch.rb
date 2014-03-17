f77fd4dccd0c9c75347e4a44f459340bfcf397af
Adrian Bautista <adrianbautista@users.noreply.github.com>
2014-03-17 10:34:45 -0400

    Update branch script

--- scripts/branch.rb
+++ scripts/branch.rb
@@ -2,8 +2,7 @@
 branches = STDIN.read.chomp
 if !branches.empty?
   branch = branches.split("\n").map(&:strip)[0].split(' ').map(&:strip)[-1]
-  second_line = branches.split("\n").map(&:strip)[1]
-  is_clean = second_line == "nothing to commit, working directory clean"
+  is_clean = branches.include?("nothing to commit, working directory clean")
   if !branches.empty?
     if is_clean
       puts " ✓ #{branch}"
