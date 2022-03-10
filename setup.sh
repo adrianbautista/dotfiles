f34cc2fbb1c7cab832352b91ba7849b3548c61e4
Adrian Bautista <adrianbautista@users.noreply.github.com>
2022-03-09 23:30:15 -0500

    Remove old dotfile syncing and clean up vimrc

--- setup.sh
+++ setup.sh
@@ -8,15 +8,9 @@
 ########## Variables
 
 DOTFILES_DIR="$HOME/dotfiles"                     # dotfiles directory
-OLD_DOTFILES_DIR="$HOME/dotfiles_old"  # old dotfiles backup directory
 CONFIG_FILES=`ls "$HOME/dotfiles/configuration_files"`     # list of files/folders to symlink in homedir
 
 ##########
-
-# create dotfiles_old in homedir
-echo -n "Creating $OLD_DOTFILES_DIR for backup of any existing dotfiles in ~ ..."
-mkdir -p $OLD_DOTFILES_DIR
-echo "done"
 
 # change to the dotfiles directory
 echo -n "Changing to the $DOTFILES_DIR directory ..."
@@ -25,8 +19,6 @@ echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for FILE in $CONFIG_FILES; do
-  echo "Moving old $FILE from ~ to $OLD_DOTFILES_DIR"
-  mv ~/.$FILE $OLD_DOTFILES_DIR/
   echo "Creating symlink to $FILE in home directory."
   ln -s $DOTFILES_DIR/configuration_files/$FILE ~/.$FILE
 done
